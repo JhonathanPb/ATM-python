a1aff532382e4b5089cd72cc2b637524dd8a2c8a
Jhonathan Pablo <jhonathan.pablo.ss321@gmail.com>
2025-08-13 21:04:23 -0300

    operacoes2

--- caixa_eletronico/funcoes_banco/operacoes.py
+++ caixa_eletronico/funcoes_banco/operacoes.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def carregar_dados():
     try:
@@ -11,4 +12,19 @@ def carregar_dados():
 def salvar_dados(dados_usuario):
     with open("dados/contas.json", "w") as arquivos:
         json.dump(dados_usuario, arquivos)
+
+def limpar_tela():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+        
+def verificar_dados(conta, numero_conta, senha):
+    if numero_conta in conta:
+        
+        if conta[numero_conta]['senha'] == senha:
+            return True
+    
+    return False
+        
   
---
+++ caixa_eletronico/main.py
@@ -0,0 +1,31 @@
+from funcoes_banco import operacoes
+
+operacoes.carregar_dados()
+
+while True:
+    operacoes.limpar_tela()
+    print('-----------------')
+    print('---ATM PY BANK---')
+    print('1. Login')
+    print('2. Sair')
+    print('-----------------')
+    
+    opcoes = input('DIGITE UMA OPÇÃO: ')
+    
+  
+    if opcoes == '1':
+        print('Digite sua Conta e Senha')
+        conta = input('Conta: ')
+        senha = input('Senha: ')
+        
+        if operacoes.
+    elif opcoes == '2':
+        print('Obrigado por Usar o ATM PY BANK!')
+        break
+    else:
+        print('Opção INVÁLIDA.')
+
+        
+
+    
+        
